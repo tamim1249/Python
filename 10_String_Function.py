d7d21275a6f43a75ee498d275e049801014b1c7b
Tamim Mahmud <143133639+tamim1249@users.noreply.github.com>
2025-06-17 02:40:45 +0600

    Create 10_String_Function.py

---
+++ 10_String_Function.py
@@ -0,0 +1,7 @@
+str="i am a coder"
+print(str.endswith('err'))
+print(str.capitalize())
+print(str.replace('i am ','Tamim is '))
+print(str.find('c'))
+print(str.find('coder'))
+print(str.count('m'))
